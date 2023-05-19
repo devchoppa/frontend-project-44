f8edc158c6c18490fe82a27a055b8a07749f7cc9
Anton Balynov <devchoppa@gmail.com>
2023-05-19 14:41:12 +0300

    Add function

---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSyns from "readline-sync";
+
+export const getName = () => {
+    const name = readlineSyns.question('May I have your name?: ');
+
+    console.log(`Hello, ${name}!`);
+}
+
