b60c7925008b88b2571f22ea11b891d8ae6b5ecc
Anton Balynov <devchoppa@gmail.com>
2023-05-20 20:36:19 +0300

    Fix and update code

--- src/games/even-games.js
+++ src/games/even-games.js
@@ -5,26 +5,19 @@ export const gameEven = () => {
   const name = readlineSyns.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let counterCorr = 0;
+
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = Math.floor(Math.random() * 30) + 1;
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSyns.question('Answer: ');
-    if (randomNumber % 2 === 0 && answer === 'yes') {
+    const correntYes = randomNumber % 2 === 0 && answer === 'yes';
+    const correntNo = randomNumber % 2 !== 0 && answer === 'no';
+    const result = randomNumber % 2 === 0 ? 'yes' : 'no';
+    if (correntYes || correntNo) {
       console.log('Corrent!');
-      counterCorr += 1;
-    } else if (randomNumber % 2 !== 0 && answer === 'no') {
-      console.log('Corrent!');
-      counterCorr += 1;
-    } else if (randomNumber % 2 === 0 && answer === 'no') {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\n Let's try again, ${name}!`);
-      break;
     } else {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\n Let's try again, ${name}!`);
-      break;
-    }
-    if (counterCorr === 3) {
-      console.log(`Congratulations, ${name}!`);
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'.\nLet's try again, ${name}!`);
     }
   }
+  return console.log(`Congratulations, ${name}!`);
 };
