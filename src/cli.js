ba249f5f5180b96366d5d45342d7bf8f23ffeb60
Anton Balynov <devchoppa@gmail.com>
2023-05-20 19:14:35 +0300

    Update

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSyns from 'readline-sync';
 
 export const getName = () => {
+  console.log('Welcome to the Brain Games!');
   const name = readlineSyns.question('May I have your name?: ');
 
   console.log(`Hello, ${name}!`);
