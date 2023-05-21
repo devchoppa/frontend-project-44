44fe9c21c0049530caab45233c58b3d9a1d43656
Anton Balynov <devchoppa@gmail.com>
2023-05-21 13:41:15 +0300

    Add new file: source for games

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// import readlineSyns from 'readline-sync';
+
+// export const mainSource = (discription) => {
+// Приветсвие
+// console.log('Welcome to the Braing Games!');
+// const name = readlineSyns.question('May I have your name? ');
+// console.log(`Hello, ${name}!`);
+// console.log(discription);
+
+// Циклы вопросов
+// for (let i = 0; i < 3; i += 1){
+//     console.log(`Question: ${}`)
+// }
+// Ответы
+// const answer = readlineSyns.question('Answer: ');
+
+// Завершение игры
+// console.log(`Congratulations, ${name}!`);
+
+// Сравнение
+// if (correct === answer) {
+//     console.log('Correct!');
+//   } else {
+//     return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.\n
+// Let's try again, ${name}!`);
+//   }
+// }
+// return console.log(`Congratulations, ${name}!`);
+// };
