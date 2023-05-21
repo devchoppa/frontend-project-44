a6cda5b1305b49f0789eda3bef52d5baee4e3513
Anton Balynov <devchoppa@gmail.com>
2023-05-21 19:19:32 +0300

    Add new game file

---
+++ src/games/gcd.js
@@ -0,0 +1,21 @@
+import { mainSource } from '../index.js';
+
+const description = 'Find the greatest common divisor of given numbers.';
+
+export const gcdGame = () => {
+  const randomNumberOne = Math.floor(Math.random() * 30) + 1;
+  const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+
+  const question = `Question: ${randomNumberOne} ${randomNumberTwo}`;
+
+  function correctAnswerGcd(numberOne, numberTwo) {
+    if (!numberTwo) {
+      return numberOne;
+    }
+    return correctAnswerGcd(numberTwo, numberOne % numberTwo);
+  }
+  const correct = correctAnswerGcd(randomNumberOne, randomNumberTwo);
+  return [question, correct];
+};
+
+mainSource(description, gcdGame);
