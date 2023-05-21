ca09ec837445de92af1091830720fc5f9accda99
Anton Balynov <devchoppa@gmail.com>
2023-05-21 15:19:14 +0300

    Fix code for source

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,38 +1,30 @@
-import readlineSyns from 'readline-sync';
+import { mainSource } from '../index.js';
+
+const discription = 'What is the result of the expression?';
 
 export const gameCalc = () => {
-  console.log('Welcome to the Braing Games!');
-  const name = readlineSyns.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('What is the result of the expression?');
+  const symbolsArr = ['+', '-', '*'];
+  const randomNumberOne = Math.floor(Math.random() * 30) + 1;
+  const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
+  const symbol = symbolsArr[Math.round(Math.random() * (symbolsArr.length - 1))];
+  const question = `Question: ${randomNumberOne} ${symbol} ${randomNumberTwo}`;
+  let correct;
 
-  for (let i = 0; i < 3; i += 1) {
-    const symbolsArr = ['+', '-', '*'];
-    const randomNumberOne = Math.floor(Math.random() * 30) + 1;
-    const randomNumberTwo = Math.floor(Math.random() * 10) + 1;
-    const symbol = symbolsArr[Math.round(Math.random() * (symbolsArr.length - 1))];
-    console.log(`Question: ${randomNumberOne} ${symbol} ${randomNumberTwo}`);
-    const answer = readlineSyns.question('Answer: ');
-    let correct;
-
-    switch (symbol) {
-      case '+':
-        correct = randomNumberOne + randomNumberTwo;
-        break;
-      case '-':
-        correct = randomNumberOne - randomNumberTwo;
-        break;
-      case '*':
-        correct = randomNumberOne * randomNumberTwo;
-        break;
-      default:
-        break;
-    }
-    if (String(correct) === answer) {
-      console.log('Correct!');
-    } else {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correct}'.\nLet's try again, ${name}!`);
-    }
+  switch (symbol) {
+    case '+':
+      correct = randomNumberOne + randomNumberTwo;
+      break;
+    case '-':
+      correct = randomNumberOne - randomNumberTwo;
+      break;
+    case '*':
+      correct = randomNumberOne * randomNumberTwo;
+      break;
+    default:
+      correct = null;
+      break;
   }
-  return console.log(`Congratulations, ${name}!`);
+  return [question, correct];
 };
+
+mainSource(discription, gameCalc);
