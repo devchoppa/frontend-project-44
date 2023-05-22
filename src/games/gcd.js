761e3bbd00bccb71f5e91c07dc040725cc2a30a8
Anton Balynov <devchoppa@gmail.com>
2023-05-22 18:44:24 +0300

    Fix code

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,18 +2,17 @@ import { mainSource } from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const correctAnswerGcd = (numberOne, numberTwo) => {
+  if (!numberTwo) {
+    return numberOne;
+  }
+  return correctAnswerGcd(numberTwo, numberOne % numberTwo);
+};
+
 export const gcdGame = () => {
   const randomNumberOne = Math.floor(Math.random() * 30) + 1;
   const randomNumberTwo = Math.floor(Math.random() * 10) + 2;
-
   const question = `Question: ${randomNumberOne} ${randomNumberTwo}`;
-
-  const correctAnswerGcd = (numberOne, numberTwo) => {
-    if (!numberTwo) {
-      return numberOne;
-    }
-    return correctAnswerGcd(numberTwo, numberOne % numberTwo);
-  };
   const correct = correctAnswerGcd(randomNumberOne, randomNumberTwo);
   return [question, correct];
 };
