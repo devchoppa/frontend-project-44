fbf9221949345a9a19df1d4c43192c90e7487f77
Anton Balynov <devchoppa@gmail.com>
2023-05-22 18:44:45 +0300

    Add prime.js game

---
+++ src/games/prime.js
@@ -0,0 +1,17 @@
+import { mainSource } from '../index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number <= 1) return false;
+  for (let i = 2; i < number; i += 1) if (number % i === 0) return false;
+  return true;
+};
+
+export const primeNumber = () => {
+  const randomNumber = Math.floor(Math.random() * 30) + 1;
+  const question = (`Question: ${randomNumber}`);
+  const correct = isPrime(randomNumber) ? 'yes' : 'no';
+  return [question, correct];
+};
+mainSource(description, primeNumber);
