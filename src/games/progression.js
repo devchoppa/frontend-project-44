cfcdd9f409c59c61d106655651cb075c1b9cc318
Anton Balynov <devchoppa@gmail.com>
2023-05-25 14:48:44 +0300

    Fix function/const names, add new const

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,25 +1,27 @@
-import mainSource from '../index.js';
+import run from '../index.js';
 
 const description = 'What number is missing in the progression?';
-
-const progression = () => {
-  const randomNumber = Math.floor(Math.random() * 20) + 1;
-  const randomProgression = Math.floor(Math.random() * 5) + 2;
-  const randomMember = Math.floor(Math.random() * 9) + 1;
-  let correct;
+const rangeProgression = 10;
+const stepProgression = 5;
+const rangeStartNumber = 20;
+const runProgression = () => {
+  const randomNumber = Math.floor(Math.random() * rangeStartNumber) + 1;
+  const randomProgression = Math.floor(Math.random() * stepProgression) + 2;
+  const randomMember = Math.floor(Math.random() * (rangeProgression - 1)) + 1;
+  let correctAnswer;
   const getArray = () => {
     const result = [];
-    for (let i = 0; i < 10; i += 1) {
+    for (let i = 0; i < rangeProgression; i += 1) {
       result.push(randomNumber + i * randomProgression);
-      correct = randomNumber + randomMember * randomProgression;
+      correctAnswer = randomNumber + randomMember * randomProgression;
     }
     result.splice(randomMember, 1, '..');
     return result.join(' ');
   };
   const question = `Question: ${getArray()}`;
-  return [question, correct];
+  return [String(question), String(correctAnswer)];
 };
 
 export default () => {
-  mainSource(description, progression);
+  run(description, runProgression);
 };
