40e87c53988dce43179515ce05133abb14cc658d
Anton Balynov <devchoppa@gmail.com>
2023-05-29 15:51:49 +0300

    Rework progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,25 +1,28 @@
 import run from '../index.js';
+import { getRandomInt, getRandomIndex } from '../utils.js';
 
 const description = 'What number is missing in the progression?';
-const rangeProgression = 10;
-const stepProgression = 5;
-const rangeStartNumber = 20;
+const lengthProgression = 10;
+
+const createProgression = (length, start, step) => {
+  const result = [];
+
+  for (let i = 0; i < length; i += 1) {
+    result.push(start + step * i);
+  }
+  return result;
+};
+
 const runProgression = () => {
-  const randomNumber = Math.floor(Math.random() * rangeStartNumber) + 1;
-  const randomProgression = Math.floor(Math.random() * stepProgression) + 2;
-  const randomMember = Math.floor(Math.random() * (rangeProgression - 1)) + 1;
-  let correctAnswer;
-  const getArray = () => {
-    const result = [];
-    for (let i = 0; i < rangeProgression; i += 1) {
-      result.push(randomNumber + i * randomProgression);
-      correctAnswer = randomNumber + randomMember * randomProgression;
-    }
-    result.splice(randomMember, 1, '..');
-    return result.join(' ');
-  };
-  const question = `Question: ${getArray()}`;
-  return [String(question), String(correctAnswer)];
+  const start = getRandomInt(1, 30);
+  const step = getRandomInt(5, 10);
+  const progression = createProgression(lengthProgression, start, step);
+  const index = getRandomIndex(progression);
+  const correctAnswer = progression[index].toString();
+  progression.splice(index, 1, '..');
+  const question = progression.join(' ');
+
+  return [question, correctAnswer];
 };
 
 export default () => {
