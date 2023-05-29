964fabface3e114590fffff47aee5e8f5b4b7787
Anton Balynov <devchoppa@gmail.com>
2023-05-29 15:50:41 +0300

    Rename const in gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,20 +1,21 @@
 import run from '../index.js';
+import { getRandomInt } from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
-const isCorrectAnswer = (numberOne, numberTwo) => {
-  if (!numberTwo) {
-    return numberOne;
+const isCorrectAnswer = (number1, number2) => {
+  if (!number2) {
+    return number1;
   }
-  return isCorrectAnswer(numberTwo, numberOne % numberTwo);
+  return isCorrectAnswer(number2, number1 % number2);
 };
 
 const runGcd = () => {
-  const randomNumberOne = Math.floor(Math.random() * 30) + 1;
-  const randomNumberTwo = Math.floor(Math.random() * 10) + 2;
-  const question = `Question: ${randomNumberOne} ${randomNumberTwo}`;
-  const correctAnswer = isCorrectAnswer(randomNumberOne, randomNumberTwo);
-  return [String(question), String(correctAnswer)];
+  const number1 = getRandomInt(1, 30);
+  const number2 = getRandomInt(2, 10);
+  const question = `${number1} ${number2}`;
+  const correctAnswer = isCorrectAnswer(number1, number2).toString();
+  return [question, correctAnswer];
 };
 
 export default () => {
