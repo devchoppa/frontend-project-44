6ecce33c42ecc0059f0b1a4221d5db428b7ef55c
Anton Balynov <devchoppa@gmail.com>
2023-06-02 12:17:09 +0300

    Update and fix even.js

--- src/games/even.js
+++ src/games/even.js
@@ -2,15 +2,18 @@ import run from '../index.js';
 import { getRandomInt } from '../utils.js';
 
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = (number) => (number % 2 === 0);
 
-const runEven = () => {
+const getTask = () => {
   const number = getRandomInt(1, 30);
-  const question = (`${number}`);
-  const correctAnswer = number % 2 === 0 ? 'yes' : 'no';
+  const question = `${number}`;
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 };
 
-export default () => {
-  run(description, runEven);
+const runEven = () => {
+  run(description, getTask);
 };
+
+export default runEven;
