56ecf73c324dded8ee2d22df44bd0c564ea39384
Anton Balynov <devchoppa@gmail.com>
2023-06-02 12:15:52 +0300

    Fix utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
-const getRandomIndex = (string) => Math.round(Math.random() * (string.length - 1));
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+const getRandomIndex = (collection) => Math.floor(Math.random() * (collection.length));
 export { getRandomInt, getRandomIndex };
