6896586afcab5bdf2253006ca088f1407a6c8450
Anton Balynov <devchoppa@gmail.com>
2023-06-02 12:17:58 +0300

    Update and fix prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,17 +5,23 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 const isPrime = (number) => {
   if (number <= 1) return false;
-  for (let i = 2; i < number; i += 1) if (number % i === 0) return false;
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
   return true;
 };
 
-const runPrime = () => {
+const getTask = () => {
   const number = getRandomInt(0, 100);
-  const question = (`${number}`);
+  const question = `${number}`;
   const correctAnswer = isPrime(number) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
-export default () => {
-  run(description, runPrime);
+const runPrime = () => {
+  run(description, getTask);
 };
+
+export default runPrime;
